bda648c31be3b1c477f944dda5e44ee39c2b7b1c
ye762 <ksanichalive@gmail.com>
2025-08-12 11:19:42 +0300

    [ye-serialization-benchmark] Add Gson and jackson.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,12 @@ group = "org.ye.serialization.benchmark"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.openjdk.jmh:jmh-core:1.37")
     kapt("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
 }
 
 repositories {
